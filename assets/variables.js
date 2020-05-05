72d271a3aeeb263f75e112021a10b80012da21ed
noerisme <azharajha5.property@gmail.com>
2020-05-05 21:17:00 +0700

    update nicknames variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["Ica", "Ica", "Ica", "Ica", "Caca"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
